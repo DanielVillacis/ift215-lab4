4feef9a1e582a0312819cc5391a2150c59415dc1
DanielVillacis <daniel1997v@gmail.com>
2022-11-11 13:03:37 -0500

    fix du space dans box produit

--- client/scripts/produit.js
+++ client/scripts/produit.js
@@ -17,12 +17,12 @@ function item_to_html(item) {
     item_detail = $('<div></div>').addClass('list-unstyled mt-3 mb-4')
         .append('<li>Qte dispo :' + item.qte_inventaire + '</li>')
         .append('<li>Categorie. : ' + item.categorie.nom + '</li>')
+        .append('</br>')
         .append('<small class="small">' + item.description + '</smalll><p class = "w-100 display-6 text-center"><i class="bi bi-cart-plus"></i></p>');
     item_body = $('<div></div>').addClass('card-body').append('<h1 class="card-title text-center"> $' + item.prix +  '</h1>').append(item_detail);
     item_card.append(item_head).append(item_body);
     return $('<div></div>').addClass('col-md-3').append(item_card);
 }
-
 
 $(function () {
 });
